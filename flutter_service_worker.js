b8838953157b431e070264ebb4896a48277a3d50
Abdelwahab <aaaaaaaaaab743@gmail.com>
2025-09-08 22:30:49 +0300

    fix: width of images container & project details link

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -27,14 +27,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "a2a20b5be2953e12495f28818b95dbf3
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "12c58683ae7d8aadce03b78e000ff552",
+"flutter_bootstrap.js": "1c13775e73bab13e87c9c9283e3ed5df",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "0782bc601e4a7929b354d4abb144e701",
 "/": "0782bc601e4a7929b354d4abb144e701",
-"main.dart.js": "04ed4a9754fd6a8fa8d3c4f6549a11c1",
+"main.dart.js": "46d17396ccd33232ac7c3ca818b9f6a0",
 "manifest.json": "94d82643ab7360537d472f16dbf7d981",
 "version.json": "696e453b73b0745922c9b06499e18514"};
 // The application shell files that are downloaded before a service worker can
